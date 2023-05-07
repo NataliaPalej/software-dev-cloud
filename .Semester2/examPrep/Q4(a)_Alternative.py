fbbdb437733be7fe0ca6fdf1a4149a5f079c6080
Natalia Palej <palejnatalia@gmail.com>
2023-05-07 16:57:12 +0100

    Q4 alternative_aggregation)

---
+++ .Semester2/examPrep/Q4(a)_Alternative.py
@@ -0,0 +1,34 @@
+class Lecturer:
+    def __init__(self, name, office):
+        self._lect_name = name
+        self._office = office
+
+    def getName(self):
+        return self._lect_name
+
+    def getOffice(self):
+        return self._office
+
+    def updateOffice(self, office):
+        self._office = office
+
+class Module(Lecturer):
+    def __init__(self, title, topic, lecturer_name, lecturer_office):
+        super().__init__(self, lecturer_name, lecturer_office)
+        self._topic = topic
+        self._title = title
+
+    def readTitle(self):
+        return self._title
+
+    def updateTitle(self, title):
+        self._title = title
+
+    def readTopic(self):
+        return self._topic
+
+    def readLecturerName(self):
+        return self._lect_name
+
+    def readLecturerOffice(self):
+        return self._office
