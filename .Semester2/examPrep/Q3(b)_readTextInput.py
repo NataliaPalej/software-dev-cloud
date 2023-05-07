af77caec88a633463301d99acd2140f90f9a8288
Natalia Palej <palejnatalia@gmail.com>
2023-05-07 15:55:11 +0100

    Read text input, write output finished

--- .Semester2/examPrep/Q3(b)_readTextInput.py
+++ .Semester2/examPrep/Q3(b)_readTextInput.py
@@ -1,19 +1,21 @@
 def main():
     infile = open('input.txt', "r")
     outfile = open('output.txt', "w")
-    age = 999
+    lowest_age = 999
     youngest = ''
     print('Name of Youngest Person is: ', file=outfile, end='')
 
     for line in infile:
         line = line.rstrip()
         list1 = line.split(' ')
-        next_name = list1[1]
-        next_age = int(list1[2])
-        if next_age < age:
-            age = next_age
-            youngest = next_name
+        name = list1[1]
+        age = int(list1[2])
+
+        if age < lowest_age:
+            lowest_age = age
+            youngest = name
     print('Name of Youngest Person is: {0}'.format(youngest))
     print(youngest, file=outfile)
+
 
 main()
